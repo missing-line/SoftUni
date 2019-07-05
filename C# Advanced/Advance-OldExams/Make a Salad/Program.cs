4509477f4ba0dc994a188941fbd8369ba753a56e
missing-line <v.milanov@protonmail.com>
2019-07-05 17:36:15 +0300

    (Demo) C# Advanced Exam - 16 June 2019

---
+++ C# Advanced/Advance-OldExams/Make a Salad/Program.cs
@@ -0,0 +1,128 @@
+﻿namespace Make_a_Salad
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<int> results = new List<int>();
+            int index = 0;
+
+            string[] vegetables = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            int[] calories = Console.ReadLine()
+                 .Split()
+                 .Select(int.Parse)
+                 .ToArray();
+
+            Stack<int> stack = new Stack<int>(calories);
+            Queue<int> queue = new Queue<int>();
+            FillVegetables(queue, vegetables);
+
+            int currCal = 0;
+            while (stack.Count != 0 && queue.Count != 0)
+            {
+                int veg = queue.Dequeue();
+                currCal = stack.Pop();
+               
+                currCal -= veg;
+
+                if (currCal <= 0)
+                {
+                    results.Add(index++);                 
+                    currCal = 0;
+                }
+                else
+                {                
+                    stack.Push(currCal);
+                }
+            }
+            if (currCal != 0)
+            {
+                results.Add(index++);
+            }
+
+            Print(queue, results, calories, queue);
+        }
+
+        private static void Print(Queue<int> queue, List<int> results, int[] calories, Queue<int> q)
+        {
+            Array.Reverse(calories);
+            List<int> salads = new List<int>();
+            for (int i = 0; i < results.Count; i++)
+            {
+                salads.Add(calories[results[i]]);
+            }
+            Console.WriteLine(string.Join(" ", salads));
+            var leftCal = calories.Skip(results.Count).ToArray();
+            if (leftCal.Length != 0)
+            {
+                Console.WriteLine(string.Join(" ", leftCal));
+            }
+
+            if (q.Count != 0)
+            {             
+                Console.WriteLine(ReverseVegetables(q));
+            }
+        }
+
+        private static string ReverseVegetables(Queue<int> q)
+        {
+            List<string> leftVeg = new List<string>();
+            var vegCal = q.ToArray();
+            for (int i = 0; i < vegCal.Length; i++)
+            {
+                if (vegCal[i] == 80)
+                {
+                    leftVeg.Add("tomato");
+
+                }
+                else if (vegCal[i] == 109)
+                {
+                    leftVeg.Add("lettuce");
+
+                }
+                else if (vegCal[i] == 136)
+                {
+                    leftVeg.Add("carrot");
+
+                }
+                else
+                {
+                    leftVeg.Add("potato");
+
+                }
+            }
+
+            return string.Join(" ", leftVeg);
+        }
+
+        private static void FillVegetables(Queue<int> queue, string[] vegetables)
+        {
+            for (int i = 0; i < vegetables.Length; i++)
+            {
+                if (vegetables[i] == "tomato")
+                {
+                    queue.Enqueue(80);
+                }
+                else if (vegetables[i] == "lettuce")
+                {
+                    queue.Enqueue(109);
+                }
+                else if (vegetables[i] == "carrot")
+                {
+                    queue.Enqueue(136);
+                }
+                else if(vegetables[i] == "potato")
+                {
+                    queue.Enqueue(215);
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/Advance-OldExams/The Garden/Program.cs
@@ -0,0 +1,141 @@
+﻿namespace The_Garden
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        private static int carrots = 0;     
+        private static int lettuce = 0;     
+        private static int potatoes = 0;    
+        private static int mole = 0;    
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[][] harvest = new string[n][];
+
+            FillMatrix(harvest);
+
+            string commands;
+
+            while ((commands = Console.ReadLine()) != "End of Harvest")
+            {
+                var arr = commands.Split(" ").ToArray();
+                string innerCommand = arr[0];
+                int row = int.Parse(arr[1]);
+                int col = int.Parse(arr[2]);
+
+                if (innerCommand == "Harvest" && ValidateCoordinates(harvest, row, col))
+                {
+                    GetVegetable(harvest, row, col);
+                }
+                else if (innerCommand == "Mole" && ValidateCoordinates(harvest, row, col))
+                {
+                    MoleCollectiong(harvest, row, col, arr[3]);
+                }
+            }
+
+            Print(harvest);
+        }
+
+        private static void Print(string[][] harvest)
+        {
+            for (int i = 0; i < harvest.Length; i++)
+            {
+                Console.WriteLine(string.Join(" ", harvest[i]).Trim());
+            }
+            Console.WriteLine($"Carrots: {carrots}");
+            Console.WriteLine($"Potatoes: {potatoes}");
+            Console.WriteLine($"Lettuce: {lettuce}");
+            Console.WriteLine($"Harmed vegetables: {mole}");
+        }
+
+        private static void MoleCollectiong(string[][] harvest, int row, int col, string dir)
+        {
+            if (dir == "right")
+            {
+                for (int i = col; i < harvest[row].Length; i += 2)
+                {
+                    if (harvest[row][i] == "C" || harvest[row][i] == "P" || harvest[row][i] == "L")
+                    {
+                        Eat(harvest, row, i);
+                    }
+                }
+            }
+            else if (dir == "up")
+            {
+
+                for (int i = row; i >= 0; i -= 2)
+                {
+                    if (ValidateCoordinates(harvest, i, col) &&
+                        (harvest[i][col] == "C" || harvest[i][col] == "P" || harvest[i][col] == "L"))
+                    {
+                        Eat(harvest, i, col);
+                    }
+                }
+            }
+            else if (dir == "left")
+            {
+                for (int i = col; i >= 0; i-=2)
+                {
+                    if (harvest[row][i] == "C" || harvest[row][i] == "P" || harvest[row][i] == "L")
+                    {
+                        Eat(harvest, row, i);
+                    }
+                }
+            }
+            else if (dir == "down")
+            {
+                for (int i = row; i < harvest.Length; i+=2)
+                {
+                    if (ValidateCoordinates(harvest, i, col) &&
+                        (harvest[i][col] == "C" || harvest[i][col] == "P" || harvest[i][col] == "L"))
+                    {
+                        Eat(harvest, i, col);
+                    }
+                }
+            }
+        }
+
+        private static void Eat(string[][] harvest, int row, int col)
+        {
+            mole++;
+            harvest[row][col] = " ";
+        }
+
+        private static void GetVegetable(string[][] harvest, int row, int col)
+        {
+            string vegetable = harvest[row][col];
+            switch (vegetable)
+            {
+                case "C":
+                    carrots++;
+                    break;
+                case "L":
+                    lettuce++;
+                    break;
+                case "P":
+                    potatoes++;
+                    break;
+                default:
+                    break;
+            }
+            harvest[row][col] = " ";
+        }
+
+        private static bool ValidateCoordinates(string[][] matrix, int row, int col)
+        {
+            return row >= 0 && row < matrix.Length && col >= 0 && col < matrix[row].Length;
+        }
+
+        private static void FillMatrix(string[][] matrix)
+        {
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                matrix[i] = Console.ReadLine().Split(" ").ToArray();
+            }
+        }
+    }
+}
