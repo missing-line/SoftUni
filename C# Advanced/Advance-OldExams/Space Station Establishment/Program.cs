b7be9f524e16036913b12669afa5cc15051a17c2
missing-line <v.milanov@protonmail.com>
2019-07-09 13:25:53 +0300

    C# Advanced Exam - 23 June 2019

---
+++ C# Advanced/Advance-OldExams/Space Station Establishment/Program.cs
@@ -0,0 +1,200 @@
+﻿namespace Space_Station_Establishment
+{
+    using System;
+    public class Program
+    {       
+        private static int startsForShip = 0;
+        private static int[] start = new int[2];
+        private static string[,] space;
+        public static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            space = new string[size, size];
+
+            FillSpace(space);
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                ExecuteCommand(command);
+            }
+        }
+
+        private static void ExecuteCommand(string command)
+        {
+            space[start[0], start[1]] = "-";
+            if (command == "right")
+            {
+                start[1]++;
+                if (!VadidateCoordinates())
+                {
+                    Console.WriteLine("Bad news, the spaceship went to the void.");
+                    Print();
+                }
+                else if (space[start[0], start[1]] == "O")
+                {
+                    space[start[0], start[1]] = "-";
+                    FindHoll();
+                    space[start[0], start[1]] = "S";
+
+                }
+                else if (space[start[0], start[1]] == "-")
+                {
+                    space[start[0], start[1]] = "S";
+                }
+                else
+                {
+                    startsForShip += int.Parse(space[start[0], start[1]]);
+                    space[start[0], start[1]] = "S";
+                }
+            }
+            else if (command == "left")
+            {
+                start[1]--;
+                if (!VadidateCoordinates())
+                {
+                    Console.WriteLine("Bad news, the spaceship went to the void.");
+                    Print();
+                }
+                else if (space[start[0], start[1]] == "O")
+                {
+                    space[start[0], start[1]] = "-";
+                    FindHoll();
+                    space[start[0], start[1]] = "S";
+
+                }
+                else if (space[start[0], start[1]] == "-")
+                {
+                    space[start[0], start[1]] = "S";
+                }
+                else
+                {
+                    startsForShip += int.Parse(space[start[0], start[1]]);
+                    space[start[0], start[1]] = "S";
+                }
+            }
+            else if (command == "up")
+            {
+                start[0]--;
+                if (!VadidateCoordinates())
+                {
+                    Console.WriteLine("Bad news, the spaceship went to the void.");
+                    Print();
+                }
+                else if (space[start[0], start[1]] == "O")
+                {
+                    space[start[0], start[1]] = "-";
+                    FindHoll();
+                    space[start[0], start[1]] = "S";
+
+                }
+                else if (space[start[0], start[1]] == "-")
+                {
+                    space[start[0], start[1]] = "S";
+                }
+                else
+                {
+                    startsForShip += int.Parse(space[start[0], start[1]]);
+                    space[start[0], start[1]] = "S";
+                }
+            }
+            else if (command == "down")
+            {
+                start[0]++;
+                if (!VadidateCoordinates())
+                {
+                    Console.WriteLine("Bad news, the spaceship went to the void.");
+                    Print();
+                }
+                else if (space[start[0], start[1]] == "O")
+                {
+                    space[start[0], start[1]] = "-";
+                    FindHoll();
+                    space[start[0], start[1]] = "S";
+
+                }
+                else if (space[start[0], start[1]] == "-")
+                {
+                    space[start[0], start[1]] = "S";
+                }
+                else
+                {
+                    startsForShip += int.Parse(space[start[0], start[1]]);
+                    space[start[0], start[1]] = "S";
+                }
+            }
+
+            if (startsForShip >= 50)
+            {
+                Console.WriteLine("Good news! Stephen succeeded in collecting enough star power!");
+                Print();
+            }
+        }
+
+        private static void FindHoll()
+        {
+            bool isFind = false;
+            for (int i = 0; i < space.GetLength(0); i++)
+            {
+               
+                for (int j = 0; j < space.GetLength(1); j++)
+                {
+                    if (space[i, j] == "O")
+                    {
+                        start[0] = i;
+                        start[1] = j;
+                        isFind = true;
+                        break;
+                    }
+                }
+                if (isFind)
+                {
+                    break;
+                }
+            }
+        }
+
+        private static void Print()
+        {
+            Console.WriteLine($"Star power collected: {startsForShip}");
+
+            for (int i = 0; i < space.GetLength(0); i++)
+            {
+                for (int j = 0; j < space.GetLength(1); j++)
+                {
+                    Console.Write(space[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Environment.Exit(1);
+        }
+
+        private static bool VadidateCoordinates()
+        {
+            int row = start[0];
+            int col = start[1];
+            return row >= 0 && row < space.GetLength(0) && col >= 0 && col < space.GetLength(1);
+        }
+
+        private static void FillSpace(string[,] space)
+        {
+            for (int i = 0; i < space.GetLength(0); i++)
+            {
+                string line = Console.ReadLine();
+                for (int j = 0; j < space.GetLength(1); j++)
+                {
+                    space[i, j] = line[j].ToString();
+
+                    if (space[i, j] == "S")
+                    {
+                        start[0] = i;
+                        start[1] = j;
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ C# Advanced/Advance-OldExams/Spaceship Crafting/Program.cs
@@ -0,0 +1,109 @@
+﻿namespace Spaceship_Crafting
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        private static int Aluminium = 0;
+        private static int CarbonFiber = 0;
+        private static int Glass = 0;
+        private static int Lithium = 0;
+        private static Queue<int> queue;
+        private static Stack<int> stack;
+        private static List<int> advanceMaterialsValue = new List<int>() { 25, 50, 75, 100 };
+        public static void Main(string[] args)
+        {
+          
+            int[] chemicals = Console.ReadLine()
+                 .Split(" ")
+                 .Select(int.Parse)
+                 .ToArray();
+
+            int[] items = Console.ReadLine()
+                 .Split(" ")
+                 .Select(int.Parse)
+                 .ToArray();
+
+            queue = new Queue<int>(chemicals);
+            stack = new Stack<int>(items);
+
+            while (stack.Count != 0 && queue.Count != 0)
+            {
+                int stackElement = stack.Pop();
+                int queueElement = queue.Dequeue();
+                int sum = stackElement + queueElement;
+
+                if (advanceMaterialsValue.Contains(sum))
+                {
+                    AddMaterial(sum);                  
+                }
+                else
+                {
+                    stackElement += 3;
+                    stack.Push(stackElement);
+                }
+
+            }
+
+            Print();
+        }
+
+        private static void AddMaterial(int sum)
+        {
+            switch (sum)
+            {
+                case 25:
+                    Glass++;
+                    break;
+                case 50:
+                    Aluminium++;
+                    break;
+                case 75:
+                    Lithium++;
+                    break;
+                case 100:
+                    CarbonFiber++;
+                    break;
+            }
+        }
+
+        private static void Print()
+        {
+            if (Aluminium == 0 || Glass == 0 || Lithium == 0 || CarbonFiber == 0)
+            {
+                Console.WriteLine("Ugh, what a pity! You didn't have enough materials to build the spaceship.");             
+            }
+            else
+            {
+                Console.WriteLine("Wohoo! You succeeded in building the spaceship!");               
+            }
+
+            if (queue.Count != 0)
+            {
+                Console.WriteLine($"Liquids left: {string.Join(", ", queue)}");
+
+            }
+            else
+            {
+                Console.WriteLine("Liquids left: none");
+            }
+            if (stack.Count != 0)
+            {
+
+                Console.WriteLine($"Physical items left: {string.Join(", ", stack)}");
+            }
+            else
+            {
+                Console.WriteLine("Physical items left: none");
+            }
+
+            Console.WriteLine($"Aluminium: {Aluminium}");
+            Console.WriteLine($"Carbon fiber: {CarbonFiber}");
+            Console.WriteLine($"Glass: {Glass}");
+            Console.WriteLine($"Lithium: {Lithium}");
+
+        }
+    }
+}
