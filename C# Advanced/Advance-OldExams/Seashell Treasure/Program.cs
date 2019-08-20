593a0a73b2cc97240571bb4c96260e60db406ce0
missing-line <v.milanov@protonmail.com>
2019-08-20 22:06:39 +0300

    C# Advanced Retake Exam - 13 August 2019

---
+++ C# Advanced/Advance-OldExams/Seashell Treasure/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Seashell_Treasure
+{
+    class Program
+    {
+        private static string[][] matrix;
+        private static List<string> collected = new List<string>();
+        private static List<string> stollen = new List<string>();
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            FillMaxtrix(n);
+
+
+            string inputCommand;
+
+            while ((inputCommand = Console.ReadLine()) != "Sunset")
+            {
+                var tokens = inputCommand
+                    .Split(' ')
+                    .ToArray();
+
+                string command = tokens[0];
+                int row = int.Parse(tokens[1]);
+                int col = int.Parse(tokens[2]);
+
+                switch (command)
+                {
+                    case "Collect":
+                        Collect(row, col);
+                        break;
+                    case "Steal":
+                        Steal(row, col, tokens[3]);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Print();
+        }
+
+        private static void Print()
+        {
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                Console.WriteLine(string.Join(" ", matrix[i]));
+            }
+            if (collected.Count > 0)
+            {
+                Console.WriteLine($"Collected seashells: {collected.Count} -> {string.Join(", ", collected)}");
+
+            }
+            else
+            {
+                Console.WriteLine($"Collected seashells: 0");
+            }
+            
+            Console.WriteLine($"Stolen seashells: {stollen.Count}");
+        }
+
+        private static void Steal(int row, int col, string position)
+        {
+            if (IsValid(row, col))
+            {
+                GetTrasure(row, col);
+
+                if (position == "up")
+                {
+                    for (int i = 1; i <= 3; i++)
+                    {
+                        if (IsValid(row - i, col))
+                        {
+                            GetTrasure(row - i, col);
+                        }                       
+                    }
+                }
+                else if (position == "down")
+                {
+                    for (int i = 1; i <= 3; i++)
+                    {
+                        if (IsValid(row + i, col))
+                        {
+                            GetTrasure(row + i, col);
+                        }
+                    }
+                }
+                else if (position == "right")
+                {
+                    for (int i = 1; i <= 3; i++)
+                    {
+                        if (IsValid(row, col + i))
+                        {
+                            GetTrasure(row, col + i);
+                        }
+                    }
+                }
+                else if (position == "left")
+                {
+                    for (int i = 1; i <= 3; i++)
+                    {
+                        if (IsValid(row, col - i))
+                        {
+                            GetTrasure(row, col - i);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void GetTrasure(int row, int col)
+        {
+            if (matrix[row][col] != "-")
+            {
+                stollen.Add(matrix[row][col]);
+                matrix[row][col] = "-";
+            }
+        }
+
+        private static void Collect(int row, int col)
+        {
+            if (IsValid(row, col))
+            {
+                if (matrix[row][col] != "-")
+                {
+                    collected.Add(matrix[row][col]);
+                    matrix[row][col] = "-";
+                }
+            }
+        }
+
+        private static void FillMaxtrix(int n)
+        {
+            matrix = new string[n][];
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+
+                matrix[i] = Console.ReadLine()
+                    .Split(' ')
+                    .ToArray();
+            }
+        }
+
+        private static bool IsValid(int row, int col)
+        {
+            return
+                row >= 0 &&
+                row < matrix.Length &&
+                col >= 0 &&
+                col < matrix[row].Length;
+        }
+    }
+}
---
+++ C# Advanced/Advance-OldExams/Summer Cocktails/Program.cs
@@ -0,0 +1,102 @@
+﻿namespace Summer_Cocktails
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Dictionary<string, int> dic = new Dictionary<string, int>();
+
+            var ingredient = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            var freshness = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> queue = new Queue<int>(ingredient);
+            Stack<int> stack = new Stack<int>(freshness);
+
+
+            while (queue.Count > 0 && stack.Count > 0)
+            {               
+                int currIngredient = queue.Peek();
+                if (currIngredient == 0)
+                {
+                    queue.Dequeue();
+                    continue;
+                }
+
+                int fresh = stack.Peek();
+
+                string cocktail = GetCocktail(fresh * currIngredient);
+
+                if (cocktail != null)
+                {
+                    if (!dic.ContainsKey(cocktail))
+                    {
+                        dic.Add(cocktail, 0);
+                    }
+                    dic[cocktail]++;                  
+                }
+                else
+                {
+                    currIngredient += 5;                   
+                    queue.Enqueue(currIngredient);
+                }
+                stack.Pop();
+                queue.Dequeue();
+            }
+
+            GetResult(queue, stack, dic);                   
+        }
+
+        private static void GetResult(Queue<int> queue, Stack<int> stack, Dictionary<string, int> dic)
+        {
+            if (queue.Count == 0 && stack.Count == 0 && dic.Count >= 4)
+            {
+                Console.WriteLine("It's party time! The cocktails are ready!");
+            }
+            else
+            {
+                Console.WriteLine("What a pity! You didn't manage to prepare all cocktails.");
+                if (queue.Count > 0)
+                {
+                    Console.WriteLine($"Ingredients left: {queue.Sum()}");
+                }
+            }
+
+            PrintCocktails(dic);
+        }
+
+        private static void PrintCocktails(Dictionary<string, int> dic)
+        {
+            foreach (var cocktail in dic.OrderBy(k => k.Key))
+            {
+                Console.WriteLine($" # {cocktail.Key} --> {cocktail.Value}");
+            }
+        }
+
+        private static string GetCocktail(int value)
+        {
+            switch (value)
+            {
+                case 150:
+                    return "Mimosa";
+                case 250:
+                    return "Daiquiri";
+                case 300:
+                    return "Sunshine";
+                case 400:
+                    return "Mojito";
+                default:
+                    return null;
+            }
+        }
+    }
+}
