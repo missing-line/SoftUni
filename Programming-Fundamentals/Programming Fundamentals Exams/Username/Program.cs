fc11c242b5df526730994b3746538f02a0738772
missing-line <v.milanov@protonmail.com>
2019-08-23 16:32:25 +0300

    Programming Fundamentals Final Exam Retake - 9 August 2019

---
+++ Programming-Fundamentals/Programming Fundamentals Exams/Username/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Linq;
+
+namespace Username
+{
+    class Program
+    {
+        private static string word;
+        static void Main(string[] args)
+        {
+            word = Console.ReadLine();
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "Sign up")
+            {
+                string[] tokens = input
+                    .Split()
+                    .ToArray();
+
+                string command = tokens[0];
+                string output = null;
+                switch (command)
+                {
+                    case "Case":
+                        output = Case(tokens[1]);
+                        break;
+                    case "Reverse":
+                        int startIndex = int.Parse(tokens[1]);
+                        int endIndex = int.Parse(tokens[2]);
+
+                        if (!(startIndex >= 0 &&
+                            endIndex > startIndex &&
+                            endIndex < word.Length))
+                        {
+                            continue;
+                        }
+
+                        output = Reverse(startIndex, endIndex);
+                        break;
+                    case "Cut":
+                        output = Cut(tokens[1]);
+                        break;
+                    case "Replace":
+                        output = Replace(tokens[1]);
+                        break;
+                    case "Check":
+                        output = Check(tokens[1]);
+                        break;
+                    default:
+                        break;
+                }
+                Console.WriteLine(output);
+            }
+        }
+
+        private static string Check(string ch)
+        {
+            if (word.Contains(ch))
+            {
+                return "Valid";
+            }
+            return $"Your username must contain {ch}.";
+        }
+
+        private static string Replace(string ch)
+        {
+            return word = word.Replace(ch, "*");
+        }
+
+        private static string Cut(string subString)
+        {
+            if (word.Contains(subString))
+            {
+                word = word.Replace(subString, null);
+                return word;
+            }
+            return $"The word {word} doesn't contain {subString}.";
+        }
+
+        private static string Reverse(int startIndex, int endIndex)
+        {
+            string getSubString = word.Substring(startIndex, (endIndex - startIndex) + 1);
+            return string.Join("", getSubString.Reverse());
+        }
+       
+        private static string Case(string typeCase)
+        {
+            if (typeCase == "lower")
+            {
+                word = word.ToLower();
+            }
+            else
+            {
+                word = word.ToUpper();
+            }
+
+            return word;
+        }
+    }
+}
