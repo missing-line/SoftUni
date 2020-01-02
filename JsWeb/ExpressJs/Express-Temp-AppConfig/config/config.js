72190b0ef1efb3941779fbbb1c9b97ff968026ec
missing-line <v.milanov@protonmail.com>
2020-01-02 19:28:54 +0200

    fix

--- JsWeb/ExpressJs/Express-Temp-AppConfig/config/config.js
+++ JsWeb/ExpressJs/Express-Temp-AppConfig/config/config.js
@@ -1,11 +1,11 @@
 const env = process.env.NODE_ENV || 'development';
 
 const config = {
-    development = {
+    development: {
         port: process.env.port || 3000,
         dbURl: 'mongodb://localhost:27107/SET_NAME_DB'
     },
-    process = {}
+    process: {}
 };
 
 module.exports = config[env];
